e21ff32f367fdaf90860897e8a85e14ea99b7fe7
ProgFadi <fadiramzi.99@gmail.com>
2021-12-04 16:03:56 +0300

    binding inuts

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,16 +1,27 @@
 import React from 'react';
 import axios from 'axios'
+
 function Login(props) {
+    const [username, setUsername] = React.useState('')
+    const [password, setPassword] = React.useState('')
     
     const login = (e)=>{
         e.preventDefault()
         console.log('logging')
+        
+        axios.post('https://website-backend.computiq.tech/api/score/auth/login',{
+            email: "info@computiq.tech",
+            password: "1234"
+        })
+        .then((response) => {
+          console.log(response)
+        });
     }
     return (
         <div>
           <form>
-              <input type="text" required/>
-              <input type="password" required/>
+              <input value={username} onChange={(e)=>setUsername(e.target.value)} type="text" />
+              <input value={password} onChange={(e)=>setPassword(e.target.value)} type="password" />
               <input type="submit" onClick={login}/>
           </form>
         </div>
